5b2cef2c793297d9a80c2006cc960e1c67022566
JaimeFRF <jaimefrfonseca03@gmail.com>
2024-10-17 19:44:32 +0100

    added cake slice

--- tp1/objects/Cake.js
+++ tp1/objects/Cake.js
@@ -1,64 +1,125 @@
-import * as THREE from 'three';
-
+import * as THREE from "three";
 
 class Cake extends THREE.Object3D {
+  constructor(app, radius, height) {
+    super();
+    this.app = app;
+    this.radius = radius;
+    this.height = height;
+    this.material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+    this.insideCakeGeometry = new THREE.PlaneGeometry(this.radius, this.height);
 
-    constructor(app, radius, height){
-        super()
-        this.app = app
-        this.radius = radius
-        this.height = height
-        this.material = new THREE.MeshBasicMaterial( {color: 0xffff00} ) 
+    const cakeTexture = new THREE.TextureLoader().load("./texture/cake.jpg");
+    const coverCakeTexture = new THREE.TextureLoader().load(
+      "./texture/cover.jpg"
+    );
 
-        const cakeTexture = new THREE.TextureLoader().load('./texture/cake.jpg');
-        const coverCakeTexture = new THREE.TextureLoader().load('./texture/cover.jpg');
+    this.cakeMaterial = new THREE.MeshStandardMaterial({
+      map: cakeTexture,
+      roughness: 1,
+      metalness: 0,
+    });
+    this.coverCakeMaterial = new THREE.MeshStandardMaterial({
+      map: coverCakeTexture,
+      roughness: 1,
+      metalness: 0,
+    });
 
-        this.cakeMaterial = new THREE.MeshStandardMaterial({
-            map: cakeTexture,
-            //roughness: 1,        
-            //metalness: 0, 
-        })
-        this.coverCakeMaterial = new THREE.MeshStandardMaterial({
-            map: coverCakeTexture,
-            //roughness: 1,        
-            //metalness: 0, 
-        })
-        
+    this.init();
+  }
 
+  init() {
+    this.buildCake();
+    this.buildSlice();
+  }
 
-        this.init()
-    }
+  buildCake() {
+    const geometry = new THREE.CylinderGeometry(
+      this.radius,
+      this.radius,
+      this.height,
+      32,
+      1,
+      false,
+      0,
+      (17 / 20) * 2 * Math.PI
+    );
+    this.cake = new THREE.Mesh(geometry, this.coverCakeMaterial);
 
-    init(){
-        this.buildCake()
-    }
+    const insideCake1 = new THREE.Mesh(
+      this.insideCakeGeometry,
+      this.cakeMaterial
+    );
+    const insideCake2 = new THREE.Mesh(
+      this.insideCakeGeometry,
+      this.cakeMaterial
+    );
 
-    buildCake(){
-        const geometry = new THREE.CylinderGeometry(this.radius, this.radius, this.height, 32, 1, false, 0, (17/20) * 2 * Math.PI)
-        this.cake = new THREE.Mesh( geometry, this.coverCakeMaterial )
+    insideCake1.rotation.y = -(Math.PI / 2);
+    insideCake1.position.z = 1;
 
-        const insideCakeGeometry1 = new THREE.PlaneGeometry(this.radius, this.height)
-        const insideCakeGeometry2 = new THREE.PlaneGeometry(this.radius * 2, this.height)
+    const angle = (3 / 20) * 2 * Math.PI;
 
-        this.insideCake1 = new THREE.Mesh(insideCakeGeometry1, this.cakeMaterial)
-        this.insideCake2 = new THREE.Mesh(insideCakeGeometry2, this.cakeMaterial)
+    insideCake2.rotation.y = Math.PI / 2 - angle;
+    insideCake2.position.set(
+      -0.5 * Math.cos(Math.PI / 2 - angle) * this.radius,
+      0,
+      0.5 * Math.sin(Math.PI / 2 - angle) * this.radius
+    );
 
-        this.insideCake1.rotation.y = -(Math.PI / 2)
-        this.insideCake1.position.z = 1
+    const cake = new THREE.Group();
+    cake.add(this.cake);
+    cake.add(insideCake1);
+    cake.add(insideCake2);
+    // this.add(cake);
+  }
 
-        this.insideCake2.rotation.y = Math.PI / 2 - ((3/20) * 2 * Math.PI)
+  buildSlice() {
+    const sliceGeometry = new THREE.CylinderGeometry(
+      this.radius,
+      this.radius,
+      this.height,
+      32,
+      1,
+      false,
+      0,
+      (3 / 20) * 2 * Math.PI
+    );
 
+    this.slice = new THREE.Mesh(sliceGeometry, this.coverCakeMaterial);
+    const insideCake1 = new THREE.Mesh(
+      this.insideCakeGeometry,
+      this.cakeMaterial
+    );
+    const insideCake2 = new THREE.Mesh(
+      this.insideCakeGeometry,
+      this.cakeMaterial
+    );
+    insideCake1.rotation.y = -(Math.PI / 2);
+    insideCake1.position.z = 1;
 
-        this.app.scene.add(this.insideCake2)
-        this.app.scene.add(this.cake)
-        this.app.scene.add(this.insideCake1)
-    }
+    const angle = -(3 / 20) * 2 * Math.PI;
 
-    draw() {
-        this.app.scene.add(this);
+    insideCake2.rotation.y = Math.PI / 2 - angle;
+    insideCake2.position.set(
+      -0.5 * Math.cos(Math.PI / 2 - angle) * this.radius,
+      0,
+      0.5 * Math.sin(Math.PI / 2 - angle) * this.radius
+    );
 
-    }
+    const slice = new THREE.Group();
+    slice.add(this.slice);
+    slice.add(insideCake1);
+    slice.add(insideCake2);
 
+    slice.rotation.y = Math.PI / 2;
+    slice.rotation.x = Math.PI / 2;
+    this.add(slice);
+  }
+
+  draw() {
+    this.app.scene.add(this);
+  }
 }
 
 export { Cake };
