a7e1334b3df60d15cbddc502873cdd96855f991c
Diogo Fernandes <up202108752@up.pt>
2024-11-01 01:11:16 +0000

    Sliding curtains

---
+++ tp1/helpers/deCasteljau.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three'
+
+class BezierCurve extends THREE.Curve {
+    constructor(points) {
+        super()
+        this.points = points
+    }
+
+    deCasteljau(t, points) {
+        while (points.length > 1) {
+            const next = []
+            for (let i = 0; i < points.length - 1; i++) {
+                const p0 = points[i]
+                const p1 = points[i + 1]
+                next.push(new THREE.Vector3().lerpVectors(p0, p1, t))
+            }
+            points = next
+        }
+        return points[0]
+    } 
+
+    getPoint(t) {
+        return this.deCasteljau(t, this.points)
+    }
+}
+
+export { BezierCurve }
